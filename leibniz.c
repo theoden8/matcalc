4776854ac89fcbe82bd96733f5be495ec5bd2376
Kirill Rodriguez <theoden8@gmail.com>
2016-09-29 00:58:34 +0100

    added leibniz.c: leibniz formula for computing pi

---
+++ leibniz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gmp.h>
+#include <mpfr.h>
+
+// 1 - 1/3 + 1/5 - 1/7 ... + (-1)^n / (2 * n + 1)
+
+main(const argc, char *argv[]) {
+	if(argc > 3)
+		return EXIT_FAILURE;
+	const
+		NO_ITERATIONS = (argc >= 2) ? atoi(argv[1]) : (1 << 18),
+		PRECISION = (argc == 3) ? atoi(argv[2]) : 20000;
+	mpfr_t sum, cur;
+	mpfr_init2(sum, PRECISION);
+	mpfr_init2(cur, PRECISION);
+
+	mpfr_set_d(sum, 0.0, MPFR_RNDD);
+	for(unsigned int i = 0; i < NO_ITERATIONS; ++i) {
+		mpfr_set_d(cur, 1.0 * (i & 1 ? -1 : 1), MPFR_RNDD);
+		mpfr_div_ui(cur, cur, 2 * i + 1, GMP_RNDU);
+		/* mpfr_printf("%d\t%.20RNf\n", i, cur); */
+		mpfr_add(sum, sum, cur, GMP_RNDD);
+	}
+	/* mpfr_printf("%.2000RNf\n", sum); */
+	mpfr_mul_ui(sum, sum, 4, GMP_RNDU);
+	mpfr_printf("%.2000RNf\n", sum);
+
+	mpfr_clear(sum);
+	mpfr_clear(cur);
+}
