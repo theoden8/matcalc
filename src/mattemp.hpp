18af4862aa3cb367e84edaf201729a1a959914c6
Kirill Rodriguez <theoden8@gmail.com>
2016-12-10 06:19:18 +0000

    added euler numbers of the second kind to mattemp.hpp

--- src/mattemp.hpp
+++ src/mattemp.hpp
@@ -97,6 +97,14 @@ template <uint N> struct Euler1 <N, 0> { declare(uint) 1; };
 template <> struct Euler1 <0, 0> { declare(uint) 1; };
 
 
+template <uint N, uint M> struct Euler2 {
+	declare(uint) (N <= M) ? 0 : (2 * N - M - 1) * Euler2<N - 1, M - 1>::n + (M + 1) * Euler2<N - 1, M>::n;
+};
+template <uint N> struct Euler2 <N, N> { declare(uint) 0; };
+template <uint M> struct Euler2 <0, M> { declare(uint) 0; };
+template <> struct Euler2 <0, 0> { declare(uint) 1; };
+
+
 void check() {
 	static_assert(Min<Factorial<6>, Factorial<3> >::n == Factorial<3>::n, "");
 	static_assert(Max<Factorial<6>, Factorial<3> >::n == Factorial<6>::n, "");
@@ -147,4 +155,9 @@ void check() {
 	static_assert(Euler1<16, 16>::n == 0, "");
 	static_assert(Euler1<16, 15>::n == 1, "");
 	static_assert(Euler1<15, 7>::n == 447538817472, "");
+	static_assert(Euler2<0, 0>::n == 1, "");
+	static_assert(Euler2<1, 0>::n == 1, "");
+	static_assert(Euler2<1, 1>::n == 0, "");
+	static_assert(Euler2<8, 8>::n == 0, "");
+	static_assert(Euler2<9, 6>::n == 11026296, "");
 }
