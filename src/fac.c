a194a31517f5bd046404af24acfb3d97187504cc
Kirill Rodriguez <theoden8@gmail.com>
2016-12-13 15:27:23 +0000

    rotas opera tenet arepa sator obfuscation

--- src/fac.c
+++ src/fac.c
@@ -5,9 +5,9 @@
 // it is here for visual comparison with some other programs
 
 #define ch(condition) ;if(!(condition))return EXIT_FAILURE;
-#define Z(x) mpz_t x;
-#define E(x) mpz_init(x);
-#define B(x) mpz_fac_ui(x, n);
-#define R(x) gmp_printf("%Zd\n", x);
-#define A(x) mpz_clear(x);
-main(int c,char**v){ch(c==2)long n=atol(v[1])ch(n>=0)Z(_)E(_)B(_)R(_)A(_)}
+#define R(x) mpz_clear(x);
+#define O(x) gmp_printf("%Sd\n", x);
+#define T(x) mpz_fac_ui(x, n);
+#define A(x) mpz_init(x);
+#define S(x) mpz_t x;
+main(int c,char**v){ch(c==2)long n=atol(v[1])ch(n>=0)S(_)A(_)T(_)O(_)R(_)}
