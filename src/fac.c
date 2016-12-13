2dcb5251f6f67802f84c4d1ad4357e94983bdb01
Kirill Rodriguez <theoden8@gmail.com>
2016-12-13 15:21:54 +0000

    now it looks ok

--- src/fac.c
+++ src/fac.c
@@ -5,4 +5,9 @@
 // it is here for visual comparison with some other programs
 
 #define ch(condition) ;if(!(condition))return EXIT_FAILURE;
-main(int c,char**v){ch(c==2)long f=atol(v[1])ch(f>=0)mpz_t fac;mpz_init(fac);mpz_fac_ui(fac, f);gmp_printf("%Zd\n", fac);mpz_clear(fac);}
+#define D(x) mpz_t x;
+#define I(x) mpz_init(x);
+#define F(x,n) mpz_fac_ui(x, n);
+#define P(x) gmp_printf("%Zd\n", x);
+#define C(x) mpz_clear(x);
+main(int c,char**v){ch(c==2)long n=atol(v[1])ch(n>=0)D(f)I(f)F(f,n)P(f)C(f)}
