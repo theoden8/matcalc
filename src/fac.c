7339837cdf689c679c1382a08b3494f6587694dd
Kirill Rodriguez <theoden8@gmail.com>
2016-12-13 15:25:27 +0000

    fac->ZEBRA

--- src/fac.c
+++ src/fac.c
@@ -5,9 +5,9 @@
 // it is here for visual comparison with some other programs
 
 #define ch(condition) ;if(!(condition))return EXIT_FAILURE;
-#define D(x) mpz_t x;
-#define I(x) mpz_init(x);
-#define F(x,n) mpz_fac_ui(x, n);
-#define P(x) gmp_printf("%Zd\n", x);
-#define C(x) mpz_clear(x);
-main(int c,char**v){ch(c==2)long n=atol(v[1])ch(n>=0)D(f)I(f)F(f,n)P(f)C(f)}
+#define Z(x) mpz_t x;
+#define E(x) mpz_init(x);
+#define B(x) mpz_fac_ui(x, n);
+#define R(x) gmp_printf("%Zd\n", x);
+#define A(x) mpz_clear(x);
+main(int c,char**v){ch(c==2)long n=atol(v[1])ch(n>=0)Z(_)E(_)B(_)R(_)A(_)}
