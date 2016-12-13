6fc1030ebda5ae4bad3cd31d7b42942f832ec47b
Kirill Rodriguez <theoden8@gmail.com>
2016-12-13 15:18:42 +0000

    obfuscated a little bit more

--- src/fac.c
+++ src/fac.c
@@ -4,5 +4,5 @@
 
 // it is here for visual comparison with some other programs
 
-#define check(condition) if(!(condition))return 1;
-main(int argc,char**argv){check(argc==2)long f=atol(argv[1]);check(f>=0)mpz_t fac;mpz_init(fac);mpz_fac_ui(fac, f);gmp_printf("%Zd\n", fac);mpz_clear(fac);}
+#define ch(condition) ;if(!(condition))return EXIT_FAILURE;
+main(int c,char**v){ch(c==2)long f=atol(v[1])ch(f>=0)mpz_t fac;mpz_init(fac);mpz_fac_ui(fac, f);gmp_printf("%Zd\n", fac);mpz_clear(fac);}
