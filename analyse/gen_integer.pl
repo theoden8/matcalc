b9f8a339d513226e3d86f8724ae6eb206f502007
Kirill Rodriguez <theoden8@gmail.com>
2016-12-29 15:43:41 +0100

    fixt gen_integers

--- analyse/gen_integer.pl
+++ analyse/gen_integer.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
 $get = (sub{return scalar @ARGV == 0 ? shift : shift @ARGV});
-print int(rand() * $get->(1e9))." " for (1..($get->(1)));
+my $max = $get->(1e9);
+print int(rand() * $max)." " for (1..((1)->$get));
 print "\n"
