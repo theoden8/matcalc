d7cd3ec1a787fd50da465bee23ce1a1ee166e63b
Kirill Rodriguez <theoden8@gmail.com>
2017-01-03 17:04:06 +0100

    added eulers totient function with cuda. great fun.

--- src/etf.hpp
+++ src/etf.hpp
@@ -15,14 +15,12 @@ template <uint N> struct ETFLoopCoprimes <1, N> { declare Coprime<1, N>::n; };
 template <uint N> struct ETF {
 	declare ETFLoopCoprimes<N - 1, N>::n;
 };
-template <> struct ETF <0> { declare 0; };
 template <> struct ETF <1> { declare 1; };
 
 
 template <uint N> superconst uint etf = ETF<N>::n;
 
 void statictest_etf() {
-	static_assert(etf<0> == 0, "");
 	static_assert(etf<1> == 1, "");
 	static_assert(etf<2> == 1, "");
 	static_assert(etf<3> == 2, "");
